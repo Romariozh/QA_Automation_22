35b57699e1d07bf9d32177ee99849fb3c269d15e
Romariozh <rvzhantalay@gmail.com>
2022-07-04 14:48:23 +0300

    Code revision and improvement. HomeWork 4_1

--- HW4/HomeWork4_1.py
+++ HW4/HomeWork4_1.py
@@ -1,29 +1,30 @@
 # Дана довільна строка. Напишіть код, який знайде в ній і віведе на екран 
 # кількість слів, які містять дві голосні літери підряд.
 
-vowels = ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
+vowels = ('a', 'e', 'i', 'o', 'u')
 
 enter_string = input('Please enter your sentence: ')
 enter_string = enter_string.strip(' ')
-list_str = enter_string.split(' ')
-letter_lst = list()
+enter_string = enter_string.lower()
+list_str = enter_string.split()
 
+letter_lst = list()
 count_right_word = 0
 
 for word in list_str:
-    if word != '':
-        word_lst = list(word)
-        # auxiliary meter
-        k = 0
-        for elem in word_lst:
-            if elem in vowels:
-                k += 1
-            else:
-                k = 0
-            if k == 2:
-                count_right_word += 1
-                print(f'Word "{word}" has more than two vowels at a stretch.')
-        letter_lst = list()
+    word_lst = list(word)
+    # auxiliary meter
+    k = 0
+    for elem in word_lst:
+        if elem in vowels:
+            k += 1
+        else:
+            k = 0
+        if k == 2:
+            count_right_word += 1
+            print(f'Word "{word}" has more than two vowels at a stretch.')
+            break
+    letter_lst = list()
 
 print('--------------------------------------------------')
 print(f'The sentence contains words with two vowels in a row.')
