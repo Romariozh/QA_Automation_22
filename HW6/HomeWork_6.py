4b504a05ef50399debda1b3b11abca767feba5dc
Romariozh <rvzhantalay@gmail.com>
2022-07-12 23:23:13 +0300

    After revising the code, template prints have been fixed. Fixed a bug in the output of 111 years

--- HW6/HomeWork_6.py
+++ HW6/HomeWork_6.py
@@ -26,14 +26,15 @@ def determ_age_in_str(age_string: int) -> str:
     }
     string_add_age = 'років'
     for key in gradient_str_age.keys():
-        if age_string % 10 in gradient_str_age[key] and age_string not in (11, 12, 13, 14):
+        if age_string % 10 in gradient_str_age[key] and age_string % 100 not in (11, 12, 13, 14):
             string_add_age = key
             break
     return string_add_age
 
+
 def cinema_cashier(client_age: int) -> str:
     string_add_age = determ_age_in_str(client_age)
-    # For create list of nice age from 11 to 999
+    # For create list of nice age like 11, 22, 33, 44 .... to 999
     element = 11
     nice_age = []
     while element <= 1000:
@@ -41,20 +42,22 @@ def cinema_cashier(client_age: int) -> str:
         element += 11
         if element % 10 == 0:
             element += 1
+        if element > 111:
+            element += 100
     # checking conditions according to the task + answer for very old people
     if client_age in nice_age:
-        response = f'О, вам {str(client_age)} {string_add_age}! Який цікавий вік!'
+        response = 'О, вам {} {}! Який цікавий вік!'
     elif client_age < 7:
-        response = f'Тобі ж {str(client_age)} {string_add_age}! Де твої батьки?'
+        response = 'Тобі ж {} {}! Де твої батьки?'
     elif client_age < 16:
-        response = f'Тобі лише {str(client_age)} {string_add_age}, а це фільм для дорослих!'
+        response = 'Тобі лише {} {}, а це фільм для дорослих!'
     elif client_age <= 65:
-        response = f'Незважаючи на те що вам {str(client_age)} {string_add_age}, білетів всеодно нема!'
+        response = 'Незважаючи на те що вам {} {}, білетів всеодно нема!'
     elif 65 < client_age < 100:
-        response = f'Вам {str(client_age)} {string_add_age}? Покажіть пенсійне посвідчення!'
+        response = 'Вам {} {}? Покажіть пенсійне посвідчення!'
     else:
-        response = f'Вам {str(client_age)} {string_add_age}, відвідування кінотеатру небезбечно для вашого здоров\'я!'
-    return response
+        response = 'Вам {} {}, відвідування кінотеатру небезбечно для вашого здоров\'я!'
+    return response.format(str(client_age), string_add_age)
 
 
 while True:
